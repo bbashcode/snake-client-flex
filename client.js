d148c2f0b9a548e66dcd24064454f8d7a38240d5
bbashcode <binbash.code@gmail.com>
2022-04-06 08:47:18 +0000

    feedback resolution - cleaned up client

--- client.js
+++ client.js
@@ -1,30 +1,34 @@
 const net = require("net");
 const {IP, PORT} = require("./constants");
 
-
+// establishes a connection with the game server
 const connect = function (){
   const conn = net.createConnection({
     host: IP,
     port: PORT 
   });
 
+  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", ()=> {
     console.log("Successfully connected to game server!");
-    conn.write("Name: MM6");
-    conn.write("Say: Heyo folks!");
   });
+
+  conn.on("connect", () => {
+    conn.write("Name: MM6");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Say: Heyo folks!");
+  })
 
   conn.on("data", (data) =>{
     console.log(data.toString());
+    conn.end();
   });
   
   return conn;
 };
-
-
-console.log("Connecting ...");
-connect();
 
 module.exports = connect;
